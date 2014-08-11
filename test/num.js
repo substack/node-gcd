3c3510b5f3221bbdeb429c406a89bb4d0b1c63be
jonschlinkert <dev@sellside.com>
2014-08-11 02:07:17 -0400

    Resolves #2. Use negation instead of strict equality.
    This allows the function to be used on arrays that may or may not have more than one element.

--- test/num.js
+++ test/num.js
@@ -2,6 +2,7 @@ var test = require('tape');
 var gcd = require('../');
 
 test('sanity values ripped from wikipedia', function (t) {
+    t.equal(gcd(4), 4);
     t.equal(gcd(48, 18), 6);
     t.equal(gcd(54, 24), 6);
     t.equal(gcd(48, 180), 12);
